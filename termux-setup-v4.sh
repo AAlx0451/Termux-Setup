4588634c6e42e5788025790e4247ac27ed222a16
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-01-22 12:21:48 +0300

    Update termux-setup-v4.sh

--- termux-setup-v4.sh
+++ termux-setup-v4.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg update && pkg upgrade
-pkg install git python python2 php unzip bash openssl openssh nano vim wget curl tsu nano cronie grep lsof android-tools gawk nodejs ncurses-utils
+pkg install git python python2 php unzip bash openssl openssh nano vim wget curl tsu nano cronie grep lsof android-tools gawk nodejs
 cd $HOME
 echo HISTSIZE=10000 >>.bashrc
 echo HISTFILESIZE=1000 >>.bashrc
