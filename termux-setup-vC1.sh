840b0f697750785ee0daff09ef1464da3185d723
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-01-22 13:11:08 +0300

    Update termux-setup-vC1.sh

--- termux-setup-vC1.sh
+++ termux-setup-vC1.sh
@@ -11,7 +11,13 @@ echo  >>.bashrc
 echo export EDITOR=nano >>.bashrc
 git clone https://github.com/AAlx0451/termux-style
 cd termux-style
-chmod +x * && bash ~/termux-style/install && termux-style && termux-style && echo So
+chmod +x *
+bash ~/termux-style/install 
+termux-style
+sleep 10
+termux-style
+sleep 10
+echo So
 echo This is It
 echo Script By AAlx
 echo Any Problems?
