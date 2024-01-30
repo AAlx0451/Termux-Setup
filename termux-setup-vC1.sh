b8be949d4f1c5568084f6a73c59f2924b618e1c8
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-01-30 13:01:30 +0300

    Update termux-setup-vC1.sh

--- termux-setup-vC1.sh
+++ termux-setup-vC1.sh
@@ -1,4 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # curl version
 # how to install? read README.md
-pkg update && pkg install git && git clone https://github.com/AAlx0451/Termux-Setup/ && cd Termux-Setup && bash termux-setup.sh && cd $HOME && rm -rf $HOME/Termux-Setup
+pkg upgrade
+pkg install git python python2 php unzip bash openssl openssh nano vim wget curl tsu nano cronie grep lsof android-tools gawk nodejs -y
+cd $HOME
+echo HISTSIZE=10000 >>.bashrc
+echo HISTFILESIZE=1000 >>.bashrc
+echo  >>.bashrc
+echo export HISTTIMEFORMAT="'%d.%m.%Y %H:%M:%S'" >>.bashrc
+echo  >>.bashrc
+echo export EDITOR=nano >>.bashrc
+git clone https://github.com/AAlx0451/termux-style
+cd termux-style
+chmod +x *
+bash ~/termux-style/install
+echo So
+echo This is It
+echo Script By AAlx
+echo Any Problems?
+echo Write to my GitHub!
+echo wait 3 seconds... && sleep 1 && echo 2... && echo 1... && sleep 1
+clear && echo  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡀⠀⠀⠀⠀⢤⡈⢦⡈⠙⠢⣀⠀⠀⠀⣠⣴⣾⡇⠀⠀⠀⠀⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢆⠀⠀⠀⠐⡝⢄⠙⢄⠀⠈⠢⡀⣼⣿⡃⢘⣿⠀⠀⠀⠀⠀⠀ && echo  ⠀⠀⠀⣾⡳⠶⣤⣀⣾⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⡀⠀⠀⠈⡄⠙⠪⣳⠀⠀⠹⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀ && echo  ⠀⠀⠀⢸⣧⣴⣿⢿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⡄⢳⡄⠀⠀⠘⣆⠀⠀⠑⢄⠀⢸⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀ && echo  ⠀⠀⠀⠀⢿⣿⡟⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⢹⡀⢻⡀⠀⠀⠘⢦⠀⠀⠀⠳⣼⣿⣿⣿⣧⣄⠀⠀⠀⠀⠀ && echo  ⠀⠀⠀⠀⠈⢿⣷⠀⠀⠀⢀⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢇⠈⣷⠀⠈⢦⡈⢳⡀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣦⡀⠀⠀ && echo  ⠀⠀⢀⣤⣶⡿⠀⠀⠀⠀⡼⠇⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣆⠈⢣⡘⡆⠀⠀⠙⡆⡙⢆⠀⠀⠀⠀⠈⠩⢍⡉⠉⠓⠀⠀ && echo  ⠀⣠⣿⠿⡿⠁⠀⠀⠀⣰⠁⠀⢰⠻⣄⡀⠀⠀⠀⠀⠀⣸⡇⢀⣿⠁⠀⠀⠙⣿⠀⠀⠀⢱⠐⠀⠳⣄⠀⠐⠒⠒⠂⠠⠔⠂⠀⠀ && echo  ⠀⠉⠀⡼⢁⠆⡆⠀⡴⠋⠀⣷⠏⠉⠹⡷⡄⢀⠀⠀⢀⠏⣇⣜⡇⠀⠀⣀⣀⣸⡆⠀⠀⢸⣾⠀⢺⣄⠙⢦⣀⣀⠀⠀⢢⠀⠀⠀ && echo  ⠀⠀⠈⠀⣜⡼⣡⣾⠀⢺⠀⡏⠀⠀⠀⠁⠈⢺⣧⠀⢸⢀⡿⠙⠡⣴⣿⣿⣿⡟⡿⠁⠀⠀⣷⡇⢸⢿⠀⠀⠻⡍⠙⠷⢀⠃⠀⠀ && echo  ⠠⠀⠀⠀⠨⠀⡇⢸⠀⠘⡄⣇⣠⣴⣶⣶⣿⣄⡘⢷⣼⡖⠃⠀⠘⠉⠿⠛⠛⠁⠁⠀⠀⠀⣿⣷⠈⣯⠀⠀⠀⡌⢆⠀⠀⠁⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⠃⢸⡆⠀⢹⣿⡿⠁⠙⠿⠟⠛⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⢠⡀⢠⠁⠀⣿⣩⣧⢻⠣⡀⠀⢱⠀⡧⠀⠀⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⠀⡏⢣⠀⢸⣹⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡼⠀⣼⣿⠁⠈⢫⣇⣹⣷⡦⣷⡖⠤⣄⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⣾⡇⠈⡆⢸⠟⠋⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⢹⡇⢠⣿⠛⡀⠀⠈⣿⣿⠈⠒⠺⣽⢷⣄⣳⡀ && echo  ⠀⠀⠀⠀⠀⠀⠈⢳⠀⢸⣏⠀⠐⣿⣆⠀⠀⠀⠀⠀⢀⣀⡤⠄⠀⠀⢀⠀⠈⠀⡿⠀⣼⡇⢀⣿⣄⣇⢻⢿⣳⠀⠀⠘⣆⠀⢻⡇ && echo  ⠀⠀⠀⠀⢀⡠⢚⣿⣆⡞⢿⡀⠀⠉⠛⠳⢄⡀⠀⠀⠘⠿⢀⣀⡀⠤⠊⠀⢀⣼⠃⢰⣿⠀⣾⣿⣌⣿⡾⠀⠙⢳⡀⠀⠈⣆⠀⠇ && echo  ⠀⠀⠀⡰⠋⡠⠋⢀⡟⢹⠀⢿⠒⣤⡀⠀⠀⠈⠑⠒⠤⢄⠀⠀⠀⠀⣠⡶⢛⡏⢠⣿⢻⡾⠃⠈⡟⢿⠃⢀⢱⡀⢧⠀⠀⠘⡆⠀ && echo  ⠀⠀⢰⢃⡎⠀⠀⠀⢳⠈⡇⠈⢣⡸⡉⢦⣀⠀⠀⠀⠀⠀⠑⣦⣴⣿⡿⠁⣼⡴⠋⠏⣼⡇⠀⠀⢳⢸⣧⠸⠀⠳⣼⡆⠀⠀⡟⠀ && echo  ⠀⠀⠸⠀⠱⡄⠀⠀⠈⣇⢹⡀⠀⣿⣽⣄⣿⢷⡄⠀⠀⠀⠀⣿⣿⠏⠀⣰⠏⠀⠀⠘⢁⠇⠀⠀⢸⠄⢻⣦⡇⠀⡟⢳⠀⠸⠁⠀ && echo  ⠀⠀⠀⠀⠀⠘⢆⠀⠀⠘⡄⢧⠀⢸⣦⠉⢻⡏⠀⠀⠀⠀⢰⡿⡏⠀⠐⠁⠀⠀⠀⠀⢸⠀⠀⠀⡼⠀⢘⣿⠁⠠⠀⡎⣰⠂⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⠈⢆⡀⠀⠘⣾⣧⠀⢿⣵⡞⠀⠀⠀⠀⠀⡟⠀⠉⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀⡴⠁⢀⠞⢹⠀⢀⠜⡰⠃⠀⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⠀⠈⢻⡀⠀⢸⡇⠀⢸⡟⠀⠀⠀⠀⠀⣸⠁⠀⡀⠀⠀⠀⠀⡠⠔⣹⠃⢀⡜⠀⡰⠋⢀⡟⠀⠎⣰⠃⠀⠀⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠘⡇⠀⡾⠁⠀⠀⠀⠀⢠⠋⡇⠀⠀⠀⠀⠀⠋⠀⠀⠁⡠⠊⢠⠊⠀⢀⠞⠀⣠⡾⠋⠀⠀⠀⠀⠀ && echo  ⠀⠀⠀⠀⠀⠀⠉⠒⠦⠜⣆⢀⣷⡞⠀⠀⠀⠀⠀⠀⡿⡄⢷⠀⠀⠀⠀⠀⠀⢀⣤⠞⢁⠔⠉⠉⢰⣋⣴⣿⠏⠁⠀⠀⠀⠀⠀⠀ && echo  ⠀⠀⠀⠀⢀⡠⠔⠂⠤⣤⣸⣿⣿⣷⣦⣄⣀⡀⠀⣸⡇⠘⢾⣆⠀⠀⠀⠀⣰⡟⢁⡴⠁⢀⣀⣴⣿⡟⢡⠏⠀⠀⠀⠀⠀⠀⠀⠀
+echo t.me/femboi_4vk
+echo  
+echo By AAlx, 2024
