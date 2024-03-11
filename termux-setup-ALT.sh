ac2eeecebd59091bdff0f57b42430c649c25a9e4
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-03-11 14:10:27 +0300

    Update termux-setup-ALT.sh

--- termux-setup-ALT.sh
+++ termux-setup-ALT.sh
@@ -1,12 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg update && pkg upgrade
-termux-setup-storage
-pkg install git python python2 php unzip bash openssl openssh nano vim wget curl tsu nano cronie grep lsof android-tools gawk nodejs
-cd $HOME
-echo HISTSIZE=10000 >>.bashrc
-echo HISTFILESIZE=1000 >>.bashrc
-echo  >>.bashrc
-echo export HISTTIMEFORMAT="'%d.%m.%Y %H:%M:%S'" >>.bashrc
-echo  >>.bashrc
-echo export EDITOR=nano >>.bashrc
-echo By AAlx, 2024
+pkg update -y && pkg upgrade -y && pkg install git python python2 php unzip bash openssl openssh nano vim wget curl tsu nano cronie grep lsof android-tools gawk nodejs -y
+termux-setup-storage 
+cd $HOME && echo '#settings'>>.bashrc && echo HISTSIZE=10000 >>.bashrc && echo HISTFILESIZE=1000 >>.bashrc && echo  >>.bashrc && echo export HISTTIMEFORMAT="'%d.%m.%Y %H:%M:%S'" >>.bashrc && echo  >>.bashrc && echo export EDITOR=nano >>.bashrc && echo  >>.bashrc && echo '#aliases'>>.bashrc
+git clone https://github.com/AAlx0451/termux-style && cd termux-style && chmod +x * && bash ~/termux-style/install && termux-style && termux-style
