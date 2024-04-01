abc08e2f159c7e978db82e63b87448bceddad275
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-04-01 12:14:31 +0300

    Delete termux-setup-ALT.sh

--- termux-setup-ALT.sh
+++
@@ -1,4 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg update -y && pkg upgrade -y && pkg install git python python2 php unzip bash openssl openssh nano vim wget curl tsu nano cronie grep lsof android-tools gawk nodejs -y
-termux-setup-storage 
-cd $HOME && echo '#settings'>>.bashrc && echo HISTSIZE=10000 >>.bashrc && echo HISTFILESIZE=1000 >>.bashrc && echo  >>.bashrc && echo export HISTTIMEFORMAT="'%d.%m.%Y %H:%M:%S'" >>.bashrc && echo  >>.bashrc && echo export EDITOR=nano >>.bashrc && echo  >>.bashrc && echo '#aliases'>>.bashrc
