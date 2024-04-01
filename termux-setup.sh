275717408abd6c1fc5223e8f5da8cbcc17fa6a32
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-04-01 11:34:55 +0300

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -17,11 +17,12 @@ echo HISTFILESIZE=1000 >>.bashrc
 echo  >>.bashrc
 echo export HISTTIMEFORMAT="'%d.%m.%Y %H:%M:%S'" >>.bashrc
 echo  >>.bashrc
-echo "Do you want to use nano or vim text editor? [Nano,vim]"
+clear
+echo "Do you want to use nano or vim text editor as default? [Nano,vim]"
 read custom_editor
-if [[ $custom_editor == "nano" || $custom_editor == "Nano" ]]; then
+if [[ $custom_editor == "nano" || $custom_editor == "Na>
   echo export EDITOR=nano >>.bashrc
-elif [[ $custom_editor == "vim" || $custom_editor == "Vim" ]]
+elif [[ $custom_editor == "vim" || $custom_editor == "V>
   echo export EDITOR=vim >>.bashrc
 else
   echo "Please, edit $HOME/.bashrc yourself"
@@ -31,6 +32,7 @@ echo  >>.bashrc
 echo '#aliases'>>.bashrc
 
 #theme add-on
+clear
 echo "Do you want to install termux-style theme add-on? [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
@@ -41,8 +43,10 @@ if [[ $input == "Y" || $input == "y" ]]; then
   termux-style
   termux-style
   rm -rf $HOME/termux-style
+elif [[ $input == "N" || $input == "n" ]]; then
+  echo Ok, skip it
 fi
 
 #end
-echo Wait 5 seconds... && sleep 1 && echo 4... && sleep 1 && echo 3... && sleep 1 && echo 2... && sleep 1 && echo 1... && sleep 1 && clear
+clear
 echo By AAlx, 2024
